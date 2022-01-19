2a88c481f3ea17362fe46cefd1ca65dcf145ccaf
Pliszka <karoliniakkjk@gmail.com>
2022-01-19 22:08:23 +0100

    Looped guestion about what to do

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from menu import MENU, resources
+import os
+
+machineOn=True
+
+clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
 
 # TODO 1 print resorces of machine
 def report():
@@ -11,11 +16,15 @@ def onlyGoodOptions():
     options=['1','2', '3', 'report' ]
     functionality=None
     while functionality not in options:
+        if functionality!=None:
+            print('Invalid option.')
         functionality = input('What would you like? (espresso(1)/latte(2)/cappuccino(3):')
+        clearConsole()
     return functionality
 # TODO 2 choose coffe or report
 # TODO 3 only correct answers in machine
 
 #machine body
-whatToDo=onlyGoodOptions()
-# TODO 4 
+while machineOn:
+    whatToDo=onlyGoodOptions()
+# TODO 4 cleaning func
