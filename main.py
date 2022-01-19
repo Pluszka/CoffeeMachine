b5ddcfa7b1d62d55c2575e21580dbd0288ab8ab4
Pliszka <karoliniakkjk@gmail.com>
2022-01-19 21:42:45 +0100

    func only good options ( what machine meant to do)

--- main.py
+++ main.py
@@ -7,5 +7,15 @@ def report():
     coffee = resources['coffee']
     print(f'Water: {water}\nMilk: {milk}\nCoffee: {coffee}')
 
+def onlyGoodOptions():
+    options=['1','2', '3', 'report' ]
+    functionality=None
+    while functionality not in options:
+        functionality = input('What would you like? (espresso(1)/latte(2)/cappuccino(3):')
+    return functionality
 # TODO 2 choose coffe or report
-report()
+# TODO 3 only correct answers in machine
+
+#machine body
+whatToDo=onlyGoodOptions()
+# TODO 4 
