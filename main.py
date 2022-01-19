94f293b67e83279dc0f72c77c0c0f4b636dc64e3
Pliszka <karoliniakkjk@gmail.com>
2022-01-19 20:59:01 +0100

    prompting resources func

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+from menu import MENU, resources
+
+# TODO 1 print resorces of machine
+def report(currentCondition):
+    water = currentCondition['water']
+    milk = currentCondition['milk']
+    coffee = currentCondition['coffee']
+    print(f'Water: {water}\nMilk: {milk}\nCoffee: {coffee}')
 
 
+report(resources)
